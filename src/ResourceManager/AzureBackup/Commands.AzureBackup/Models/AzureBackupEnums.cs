fa10bb977b8e4db32e4596b2b5b95cd7b0f909c5
pragrawa <pragrawa@microsoft.com>
2015-07-26 03:36:33 +0530

    LTR Policy

--- src/ResourceManager/AzureBackup/Commands.AzureBackup/Models/AzureBackupEnums.cs
+++ src/ResourceManager/AzureBackup/Commands.AzureBackup/Models/AzureBackupEnums.cs
@@ -103,8 +103,10 @@ namespace Microsoft.Azure.Commands.AzureBackup.Models
     public enum RetentionType
     {
         Invalid = 0,
-        Days = 1,
-        Weeks = 2
+        Daily = 1,
+        Weekly = 2,
+        Monthly = 3,
+        Yearly = 4
     }
 
     public enum WorkloadType
@@ -117,5 +119,11 @@ namespace Microsoft.Azure.Commands.AzureBackup.Models
     {
         Invalid = 0,
         Full = 1
+    }
+
+    public enum RetentionFormat
+    {
+        Daily,
+        Weekly
     }
 }
