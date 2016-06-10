032a5330526e1c19d8af266a828674f499f626e0
dragonfly91 <anudeeb@microsoft.com>
2016-06-10 15:33:12 +0530

    [RS Backup] Fix for Bug 7797781:Rename Name filter to FriendlyName in Get-RSContainer Cmdlet

--- src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.Providers/Providers/IaasVmPsBackupProvider.cs
+++ src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.Providers/Providers/IaasVmPsBackupProvider.cs
@@ -431,6 +431,7 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.ProviderModel
             Models.BackupManagementType? backupManagementTypeNullable = 
                 (Models.BackupManagementType?)this.ProviderData[ContainerParams.BackupManagementType];
             string name = (string)this.ProviderData[ContainerParams.Name];
+            string friendlyName = (string)this.ProviderData[ContainerParams.FriendlyName];
             string resourceGroupName = (string)this.ProviderData[ContainerParams.ResourceGroupName];
             ContainerRegistrationStatus status = 
                 (ContainerRegistrationStatus)this.ProviderData[ContainerParams.Status];
@@ -440,10 +441,22 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.ProviderModel
                 ValidateAzureVMBackupManagementType(backupManagementTypeNullable.Value);
             }
 
+            string nameQueryFilter = friendlyName;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                Logger.Instance.WriteWarning(Resources.GetContainerNameParamDeprecated);
+
+                if (string.IsNullOrEmpty(friendlyName))
+                {
+                    nameQueryFilter = name;
+                }
+            }
+
             ProtectionContainerListQueryParams queryParams = new ProtectionContainerListQueryParams();
 
             // 1. Filter by Name
-            queryParams.FriendlyName = name;
+            queryParams.FriendlyName = nameQueryFilter;
 
             // 2. Filter by ContainerType
             queryParams.BackupManagementType =
